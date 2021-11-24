1e67771cfdf918da0ee56938bc07b29b6f881e5f
CalokTI <72436270+CalokTI@users.noreply.github.com>
2021-11-24 11:13:03 +0100

    ?

---
+++ src/main/resources/static/scripts/createUser.js
@@ -0,0 +1,28 @@
+const baseUrl = "localhost:8080";
+
+document.getElementById('create-user-button').addEventListener('click', createUser);
+
+function createUser(){
+    const name  = document.getElementById('user-email').value;
+    const email = document.getElementById('user-email').value;
+
+    const user = {
+        name:name,
+        email:email
+    }
+
+    fetch(baseUrl + "/users", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+        if(response.status === 200){
+            console.log(response.status + "ok")
+        }else{
+            console.log(response.status + "nope");
+        }
+    });
+}

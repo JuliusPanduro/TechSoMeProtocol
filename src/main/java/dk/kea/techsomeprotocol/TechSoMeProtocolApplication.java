3c9e1ce376893769acf3b5faf26726e4760095c9
JuliusPanduro <j.panduro.r@hotmail.com>
2021-11-24 09:45:34 +0100

    Initial commit

---
+++ src/main/java/dk/kea/techsomeprotocol/TechSoMeProtocolApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.techsomeprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSoMeProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSoMeProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/techsomeprotocol/TechSoMeProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.techsomeprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSoMeProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
